a6a7d5895abc6e1ca564bbb6ef305f13ca5dec93
jc972539 <mark.campbell@my.jcu.edu.au>
2024-09-29 23:44:22 +1000

    Amend program code to use functions

---
+++ prac_02/temperatures.py
@@ -0,0 +1,33 @@
+"""Temperature conversion"""
+
+def main():
+    """Interchange given temperature"""
+    MENU = """C - Convert Celsius to Fahrenheit
+    F - Convert Fahrenheit to Celsius
+    Q - Quit"""
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            fahrenheit = celsius_to_fahrenheit(celsius)
+            print(f"Result: {fahrenheit:.2f} F")
+        elif choice == "F":
+            fahrenheit = float(input("Fahrenheit: "))
+            celsius = fahrenheit_to_celsius(fahrenheit)
+            print(f"Result: {celsius:.2f} C")
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+def celsius_to_fahrenheit(celsius):
+    """Converts celsius value to fahrenheit"""
+    return celsius * 9.0 / 5 + 32
+
+def fahrenheit_to_celsius(fahrenheit):
+    """Converts fahrenheit value to celsius"""
+    return 5 / 9 * (fahrenheit - 32)
+
+main()
