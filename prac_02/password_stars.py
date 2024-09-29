c0fb2cad64df6428e0c4f11abea6c21e70773cb2
jc972539 <mark.campbell@my.jcu.edu.au>
2024-09-29 21:10:52 +1000

    Update password checking program with inclusion of functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,20 @@
-password_length = 8
-password = input("Enter password: ")
-while len(password) < password_length:
-    print("Password does not meet the minimum requirements")
+"""Password checking program"""
+
+def main():
+    """Run program"""
+    password_length = 8
+    get_password(password_length)
+    print_asterisks(password_length)
+
+def print_asterisks(password_length):
+    """Print asterisks"""
+    print("*" * password_length)
+
+def get_password(password_length):
+    """Get input with error-checking"""
     password = input("Enter password: ")
-print("*" * password_length)
+    while len(password) < password_length:
+        print("Password does not meet the minimum requirements")
+        password = input("Enter password: ")
+
+main()
